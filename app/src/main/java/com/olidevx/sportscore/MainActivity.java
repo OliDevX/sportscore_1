0562efa5effbc2bb1106e684f17ea655915b0928
OliDevelopments <armando.ibarraran@gmail.com>
2016-04-15 11:08:43 -0500

    Adding OnClickListener to home_layout

--- app/src/main/java/com/olidevx/sportscore/MainActivity.java
+++ app/src/main/java/com/olidevx/sportscore/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.Layout;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -17,6 +18,17 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        // Set listener to home_layout
+        View mLayoutHome = (View) findViewById(R.id.home_layout);
+
+        mLayoutHome.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                // Does Nothing yet;
+            }
+        });
+
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
