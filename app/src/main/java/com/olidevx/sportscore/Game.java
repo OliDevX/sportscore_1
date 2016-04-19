790664983b7cdfcb7db8e36b39c85cb58ae7c4d0
OliDevelopments <armando.ibarraran@gmail.com>
2016-04-19 16:36:27 -0500

    Method ScoreChange() on Game class was updated (still some todos)

--- app/src/main/java/com/olidevx/sportscore/Game.java
+++ app/src/main/java/com/olidevx/sportscore/Game.java
@@ -36,12 +36,15 @@ public class Game {
     }
 
     public void UpdateDisplays(){
+
+        // Updates the Display holders with method SetPoints with the current Team Points
         mDisplay1.SetPoints(mTeam1.mPoints);
         mDisplay2.SetPoints(mTeam2.mPoints);
+
         // mDisplaySet.SetPoints();
         // TODO: Complete code for Set - Create Set Class
 
-
+        // Passes info from the Display classes to the Game class
         mDisp1_digit1 = mDisplay1.Digit_1;
         mDisp1_digit2 = mDisplay1.Digit_2;
         mDisp1_digit3 = mDisplay1.Digit_3;
@@ -54,8 +57,38 @@ public class Game {
         mDispSet_digit2 = mDisplaySet.Digit_2;
     }
 
-    // ScoreChange method is created to make scorechanges happen and subsecuently trigger all other actions in Team and Display classes
+    /* ScoreChange method is created to make score changes happen
+    * and then trigger all other actions in Team and Display classes
+    * int_score_id - int variable to determine the team or set to modify
+    * boolean - if TRUE then increase, if FALSE then decrease
+     */
     public void ScoreChange(int int_score_id, boolean bool_increase){
+        if ((int_score_id > 0) && (int_score_id < 3)){
+            switch (int_score_id) {
+                case 0: break;
+
+                // Case 1 indicates team 1 increases or decreases points
+                case 1:
+                    if (bool_increase){
+                        mTeam1.onIncreasePoint();
+                    } else {
+                        // mTeam1.onDecreasePoint();
+                    }
+                    break;
+
+                // Case 2 indicates team 2 increases or decreases points
+                case 2:
+                    if (bool_increase){
+                        mTeam2.onIncreasePoint();
+                    } else {
+                        // mTeam2.onDecreasePoint;
+                    }
+                    break;
+            }
+
+            // After changing the points
+            UpdateDisplays();
+        }
         // TODO: Complete this section
     }
 }
