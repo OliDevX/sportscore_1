be3bafc7be9e21c182b40026bb3805494fc1447e
OlidevX <armando.ibarraran@gmail.com>
2016-04-27 15:27:08 -0500

    Cleaning imports that were not used in mainactivity.java

--- app/src/main/java/com/olidevx/sportscore/MainActivity.java
+++ app/src/main/java/com/olidevx/sportscore/MainActivity.java
@@ -5,15 +5,10 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.text.Layout;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
-
-import junit.framework.Test;
-
-import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -156,6 +151,7 @@ public class MainActivity extends AppCompatActivity {
         mDisplay2_third_digit.setText(mGame.mDisp2_digit3);
 
     }
+
 
 
 }
